86c540e1f2f3aec287f098906a324f1f10852b69
Om Singh <omsingh.tech1@gmail.com>
2026-08-21 02:07:40 +0530

    Homework

---
+++ InsertElementAtFirst.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5] = {1, 2, 3, 4};
+    int n = 4;
+    int value;
+
+    printf("Array before insertion: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    printf("Enter the value to insert: ");
+    scanf("%d", &value);
+
+    for (int i = n; i > 0; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[0] = value;
+    n++;
+
+    printf("Array after insertion: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ InsertElementAtLast.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5]= {1, 2, 3, 4, 5};
+    int n = 4;
+    int value;
+
+    printf("Array before insertion: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    printf("Enter the value to insert: ");
+    scanf("%d", &value);
+
+    arr[n] = value;
+    n++;
+
+    printf("Array after insertion: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
